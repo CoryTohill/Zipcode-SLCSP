28ef34a575ab3617faf34dc394ca90ab7262701e
Cory Tohill <corytohill@lasso.io>
2024-01-30 19:01:26 -0600

    create a dict with zipcode keys and a set of rate areas for values

--- get_slcsp_from_zipcodes.py
+++ get_slcsp_from_zipcodes.py
@@ -23,8 +23,19 @@ def read_csv(file_path):
 plans_file_path = 'plans.csv'
 slcsps_file_path = 'slcsp.csv'
 zips_file_path = 'zips.csv'
-
-
 plan_data = read_csv(plans_file_path)
 slcsp_data = read_csv(slcsps_file_path)
 zipcode_data = read_csv(zips_file_path)
+
+
+# Filter the zipcode data to only include the zipcodes needed
+zipcodes_needed = [slcsp['zipcode'] for slcsp in slcsp_data]
+filtered_zipcode_data = [zip_row for zip_row in zipcode_data if zip_row['zipcode'] in zipcodes_needed]
+
+
+# Create a dictionary where the keys are the zipcodes
+# and the values are a set of tuples which contain the state and rate area number
+zipcode_rate_areas = {}
+
+for zid_data in filtered_zipcode_data:
+    zipcode_rate_areas.setdefault(zid_data['zipcode'], set()).add((zid_data['state'], zid_data['rate_area']))
